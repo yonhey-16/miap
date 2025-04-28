52e69a5a2220edb70419b1067a0c205ba25508ff
yonhey-16 <yonheybonilla@gmail.com>
2025-04-28 03:25:51 +0000

    miap

--- JS/sw.js
+++ JS/sw.js
@@ -13,7 +13,6 @@ self.addEventListener('install', e => {
         '/js/detalle.js',
         '/icons/icon-192.png',
         '/icons/icon-512.png',
-        '/js/tvmaze-api.js', 
       ]);
     })
   );
